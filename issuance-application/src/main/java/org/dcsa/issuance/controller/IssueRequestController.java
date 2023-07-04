16bb1351f536d904ae25b9dfda2e0b059cfbd92a
Niels Thykier <nit@globeteam.com>
2023-07-04 09:50:00 +0200

    Ensure each party code list provider is only used at most once
    Signed-off-by: Niels Thykier <nit@globeteam.com>

--- issuance-application/src/main/java/org/dcsa/issuance/controller/IssueRequestController.java
+++ issuance-application/src/main/java/org/dcsa/issuance/controller/IssueRequestController.java
@@ -4,10 +4,15 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.dcsa.issuance.service.IssueRequestService;
 import org.dcsa.issuance.transferobjects.IssuanceRequestTO;
+import org.dcsa.issuance.transferobjects.IssuePartyTO;
+import org.dcsa.issuance.transferobjects.enums.PartyCodeListProviderTO;
 import org.dcsa.skernel.errors.exceptions.ConcreteRequestErrorMessageException;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.EnumSet;
+import java.util.Set;
 
 @Validated
 @RestController
@@ -19,6 +24,7 @@ public class IssueRequestController {
   @PutMapping(path = "${spring.application.iss-context-path}" + "/issue-ebls")
   @ResponseStatus(HttpStatus.NO_CONTENT)
   public void newIssuanceRequest(@Valid @RequestBody IssuanceRequestTO issuanceRequestTO) {
+    validateIssueParty(issuanceRequestTO.issueTo());
     if (issueRequestService.hasPendingReferenceForTransportDocument(issuanceRequestTO)) {
       // Do a check here to provide a nice error message.
       //
@@ -31,5 +37,20 @@ public class IssueRequestController {
       );
     }
     issueRequestService.createIssuanceRequest(issuanceRequestTO);
+  }
+
+  private void validateIssueParty(IssuePartyTO issuePartyTO) {
+    var supportingPartyCodeTOs = issuePartyTO.supportingPartyCodes();
+    if (supportingPartyCodeTOs == null) {
+      return;
+    }
+    Set<PartyCodeListProviderTO> codeListProviderSet = EnumSet.noneOf(PartyCodeListProviderTO.class);
+    for (var supportingPartyCode : supportingPartyCodeTOs) {
+      if (!codeListProviderSet.add(supportingPartyCode.partyCodeListProvider())) {
+        throw ConcreteRequestErrorMessageException.invalidInput("The \"issueTo\" party had two supporting party codes"
+          + " with code list provider \"" + supportingPartyCode.partyCodeListProvider() + "\". That code list provider"
+          + " must be used at most once.");
+      }
+    }
   }
 }
